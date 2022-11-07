c6e59e816873974969342640349dd579370f0f9b
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 19:36:37 +0600

    fix: error if files-copy doesn't exist

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,42 +3,49 @@ const path = require('path');
 const dir = 'files-copy';
 
 function copyDir() {
+  function createCopyFolder() {
+    // create copy folder
+    fs.promises.mkdir(path.join(__dirname, dir), {recursive: true})
+    .then(() => {
+      console.log(`${dir} is created`);
+
+      // scan origin
+      fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
+        .then(files => {
+
+          // copy all files
+          for (let i = 0; i < files.length; i++) {
+            
+            fs.promises.copyFile(path.join(__dirname, 'files', files[i].name), path.join(__dirname, dir, files[i].name))
+              .then(() => {
+                console.log('file is copied succesfully');
+              })
+            
+          }
+        })
+
+    })
+  }
+
   // check if copied folder exists
   fs.promises.access(path.join(__dirname, dir))
     .then(() => {
-      console.log(`${dir} folder exists, deleting...`)
+      console.log(`${dir} folder exists, deleting...`);
       // delete it if it exists, to remove files if they were deleted in origin
       fs.promises.rm(path.join(__dirname, dir), {recursive: true})
         .then(() => {
           console.log(`${dir} is succesfully deleted`);
 
-          // create copy folder
-          fs.promises.mkdir(path.join(__dirname, dir), {recursive: true})
-            .then(() => {
-              console.log('dir is created');
-
-              // scan origin
-              fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
-                .then(files => {
-
-                  // copy all files
-                  for (let i = 0; i < files.length; i++) {
-                    
-                    fs.promises.copyFile(path.join(__dirname, 'files', files[i].name), path.join(__dirname, dir, files[i].name))
-                      .then(() => {
-                        console.log('file is copied succesfully');
-                      })
-                    
-                  }
-                })
-
-            })
+          createCopyFolder();
 
         })
         
     })
+    .catch(() => {
+      console.log(`${dir} doesn't exist`);
+      createCopyFolder();
+    })
 
-  
 }
 
 copyDir();
