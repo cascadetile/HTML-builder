32bbc01ffd63232777b476fc5da84c7edca7e5fe
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 14:59:03 +0600

    feat: exercise 5 solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectDist = 'project-dist';
+const bundle = 'bundle.css';
+const bundlePath = path.join(__dirname, projectDist, bundle);
+
+fs.promises.writeFile(bundlePath, '')
+  .then(() => {
+    console.log('creating bundle.css');
+  })
+
+let writeStream = fs.createWriteStream(bundlePath);
+
+let cssFiles;
+
+fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+  .then(files => {
+
+    cssFiles = files.filter((file) => path.parse(file.name).ext === '.css');
+    for (let i = 0; i < cssFiles.length; i++) {
+      let readStream = fs.createReadStream(path.join(__dirname, 'styles', cssFiles[i].name));
+
+      readStream.pipe(writeStream);
+    }
+    
+  })
