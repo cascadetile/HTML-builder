172db90f941833bdaa7cd85be7788d3feb8796da
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 17:07:05 +0600

    fix: remove copy folder in the beginning

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,24 +3,42 @@ const path = require('path');
 const dir = 'files-copy';
 
 function copyDir() {
-
-  fs.promises.mkdir(path.join(__dirname, dir), {recursive: true})
+  // check if copied folder exists
+  fs.promises.access(path.join(__dirname, dir))
     .then(() => {
-      console.log('dir is created');
-    });
+      console.log(`${dir} folder exists, deleting...`)
+      // delete it if it exists, to remove files if they were deleted in origin
+      fs.promises.rm(path.join(__dirname, dir), {recursive: true})
+        .then(() => {
+          console.log(`${dir} is succesfully deleted`);
 
-  fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
-    .then(files => {
+          // create copy folder
+          fs.promises.mkdir(path.join(__dirname, dir), {recursive: true})
+            .then(() => {
+              console.log('dir is created');
 
-      for (let i = 0; i < files.length; i++) {
+              // scan origin
+              fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
+                .then(files => {
+
+                  // copy all files
+                  for (let i = 0; i < files.length; i++) {
+                    
+                    fs.promises.copyFile(path.join(__dirname, 'files', files[i].name), path.join(__dirname, dir, files[i].name))
+                      .then(() => {
+                        console.log('file is copied succesfully');
+                      })
+                    
+                  }
+                })
+
+            })
+
+        })
         
-        fs.promises.copyFile(path.join(__dirname, 'files', files[i].name), path.join(__dirname, dir, files[i].name))
-          .then(() => {
-            console.log('file is copied succesfully');
-          })
-        
-      }
     })
+
+  
 }
 
 copyDir();
