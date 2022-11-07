195eafeef4e9a96494d604d0d27d4c6a5cddc9a4
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 19:21:22 +0600

    fix: wait until all templates are replaced

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,88 +5,102 @@ const projectDist = 'project-dist';
 const bundle = 'style.css';
 const bundlePath = path.join(__dirname, projectDist, bundle);
 
-fs.promises.mkdir(path.join(__dirname, projectDist), {recursive: true})
+fs.promises.access(path.join(__dirname, projectDist))
   .then(() => {
-    console.log('dir is created');
-  });
+    console.log(`${projectDist} folder exists, deleting...`);
+    fs.promises.rm(path.join(__dirname, projectDist), {recursive: true})
+      .then(() => {
+        console.log(`${projectDist} is succesfully deleted`);
 
-fs.promises.writeFile(bundlePath, '')
-  .then(() => {
-    console.log(`creating ${bundle}`);
-  })
+        fs.promises.mkdir(path.join(__dirname, projectDist), {recursive: true})
+          .then(() => {
+            console.log(`${projectDist} directory is created`);
 
-let writeStream = fs.createWriteStream(bundlePath);
+            fs.promises.writeFile(bundlePath, '')
+              .then(() => {
+                console.log(`creating ${bundle}`);
+              })
 
-let cssFiles;
+            let writeStream = fs.createWriteStream(bundlePath);
 
-// bundle styles
-fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
-  .then(files => {
+            let cssFiles;
 
-    cssFiles = files.filter((file) => path.parse(file.name).ext === '.css');
-    for (let i = 0; i < cssFiles.length; i++) {
-      let readStream = fs.createReadStream(path.join(__dirname, 'styles', cssFiles[i].name));
+            // bundle styles
+            fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+              .then(files => {
 
-      readStream.pipe(writeStream);
-    }
+                cssFiles = files.filter((file) => path.parse(file.name).ext === '.css');
+                for (let i = 0; i < cssFiles.length; i++) {
+                  let readStream = fs.createReadStream(path.join(__dirname, 'styles', cssFiles[i].name));
 
-  })
+                  readStream.pipe(writeStream);
+                }
 
-fs.promises.mkdir(path.join(__dirname, projectDist, 'assets'), {recursive: true})
-  .then(() => {
+              })
 
-    fs.promises.readdir(path.join(__dirname, 'assets'), { withFileTypes: true })
-      .then(dirs => {
 
-        // inside each directory
-        for (let i = 0; i < dirs.length; i++) {
+              fs.promises.mkdir(path.join(__dirname, projectDist, 'assets'), {recursive: true})
+              .then(() => {
+            
+                fs.promises.readdir(path.join(__dirname, 'assets'), { withFileTypes: true })
+                  .then(dirs => {
+            
+                    // inside each directory
+                    for (let i = 0; i < dirs.length; i++) {
+            
+                      // create a clone of assets dirs
+                      fs.promises.mkdir(path.join(__dirname, projectDist, 'assets', dirs[i].name), {recursive: true})
+                        .then(() => {
+                          console.log(`${dirs[i].name} directory is created`);
+            
+                          // read all files of curr dir
+                          fs.promises.readdir(path.join(__dirname, 'assets', dirs[i].name), { withFileTypes: true })
+                            .then((files) => {
+            
+                              for (let j = 0; j < files.length; j++) {
+                                fs.promises.copyFile(path.join(__dirname, 'assets', dirs[i].name, files[j].name), path.join(__dirname, projectDist, 'assets', dirs[i].name, files[j].name))
+                                  .then(() => {
+                                    console.log(`${files[j].name} is copied succesfully`);
+                                  })
+                              }
+            
+                            })
+                        });
+                      
+                      
+                    }
+                  })
+            
+              })
+            
+            // replace templates with components
+            fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf8')
+              .then((data) => {
+                
+                let mustaches = data.match(/{{.+}}/g);
+                
+                let template = data;
 
-          // create a clone of assets dirs
-          fs.promises.mkdir(path.join(__dirname, projectDist, 'assets', dirs[i].name), {recursive: true})
-            .then(() => {
-              console.log('dir is created');
+                replaceTemplates(mustaches, template);
+            
+              })
 
-              // read all files of curr dir
-              fs.promises.readdir(path.join(__dirname, 'assets', dirs[i].name), { withFileTypes: true })
-                .then((files) => {
-
-                  for (let j = 0; j < files.length; j++) {
-                    fs.promises.copyFile(path.join(__dirname, 'assets', dirs[i].name, files[j].name), path.join(__dirname, projectDist, 'assets', dirs[i].name, files[j].name))
-                      .then(() => {
-                        console.log('file is copied succesfully');
-                      })
-                  }
-
-                })
-            });
-          
-          
-        }
+          });
       })
-
   })
 
-fs.promises.readFile(path.join(__dirname, 'template.html'), 'utf8')
-  .then((data) => {
-    
-    let mustaches = data.match(/{{.+}}/g);
-    
-    let template = data;
+async function replaceTemplates(mustaches, template) {
+  for (let i = 0; i < mustaches.length; i++) {
+    let fileName = mustaches[i].replace(/{{/, '').replace(/}}/, '');
 
-    for (let i = 0; i < mustaches.length; i++) {
-      let fileName = mustaches[i].replace(/{{/, '').replace(/}}/, '');
+    let component = await fs.promises.readFile(path.join(__dirname, 'components', fileName + '.html'), 'utf8');
 
-      fs.promises.readFile(path.join(__dirname, 'components', fileName + '.html'), 'utf8')
-        .then((component) => {
-          let regex = new RegExp(mustaches[i]);
-          template = template.replace(regex, component);
+    let regex = new RegExp(mustaches[i]);
+    template = template.replace(regex, component);
 
-          if (i === mustaches.length - 1) {
-            fs.promises.writeFile(path.join(__dirname, projectDist, 'index.html'), template);
-          }
-        })
-
+    if (i === mustaches.length - 1) {
+      fs.promises.writeFile(path.join(__dirname, projectDist, 'index.html'), template);
     }
 
-
-  })
+  }
+}
