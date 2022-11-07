97d63c2f2a4ec38d9855ed7f4fc16287008ac59f
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 14:58:22 +0600

    feat: exercise 4 solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const dir = 'files-copy';
 
+function copyDir() {
+
+  fs.promises.mkdir(path.join(__dirname, dir), {recursive: true})
+    .then(() => {
+      console.log('dir is created');
+    });
+
+  fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
+    .then(files => {
+
+      for (let i = 0; i < files.length; i++) {
+        
+        fs.promises.copyFile(path.join(__dirname, 'files', files[i].name), path.join(__dirname, dir, files[i].name))
+          .then(() => {
+            console.log('file is copied succesfully');
+          })
+        
+      }
+    })
+}
+
+copyDir();
