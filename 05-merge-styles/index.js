a4152bfbd2135ea9a3f78836730efd034d3ff891
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-11-07 17:41:25 +0600

    fix: make code more robust

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,20 +8,22 @@ const bundlePath = path.join(__dirname, projectDist, bundle);
 fs.promises.writeFile(bundlePath, '')
   .then(() => {
     console.log('creating bundle.css');
-  })
 
-let writeStream = fs.createWriteStream(bundlePath);
+    let writeStream = fs.createWriteStream(bundlePath);
 
-let cssFiles;
+    let cssFiles;
 
-fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
-  .then(files => {
+    fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true })
+      .then(files => {
 
-    cssFiles = files.filter((file) => path.parse(file.name).ext === '.css');
-    for (let i = 0; i < cssFiles.length; i++) {
-      let readStream = fs.createReadStream(path.join(__dirname, 'styles', cssFiles[i].name));
+        cssFiles = files.filter((file) => path.parse(file.name).ext === '.css');
+        for (let i = 0; i < cssFiles.length; i++) {
+          console.log(`reading file ${files[i].name}`);
+          let readStream = fs.createReadStream(path.join(__dirname, 'styles', cssFiles[i].name));
 
-      readStream.pipe(writeStream);
-    }
-    
+          readStream.pipe(writeStream);
+        }
+        
+      })
+
   })
